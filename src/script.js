81d607e9eb2b2d9c651c7291774b15acacdfd936
Faay97 <101605402+Faay97@users.noreply.github.com>
2022-04-04 22:59:50 -0400

    Launch files
    HTML file created
    JS file created
    CSS file created

---
+++ src/script.js
@@ -0,0 +1,60 @@
+function formatDate(now) {
+  let dates = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let date = dates[now.getDay()];
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${date} ${hour}:${minutes}`;
+}
+let currentTime = new Date();
+let day = document.querySelector(".day");
+
+day.innerHTML = formatDate(currentTime);
+
+function searching(event) {
+  event.preventDefault();
+  let search = document.querySelector("#search-input").value;
+  let apiKey = "156d56571be94f1383a53abf8e9ae72f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`;
+
+  axios.get(`${apiUrl}&units=metric`).then(temperature);
+}
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", searching);
+
+function temperature(response) {
+  document.querySelector("#city-display").innerHTML = response.data.name;
+  document.querySelector("#number").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrieveLocation);
+}
+function retrieveLocation(position) {
+  //position.preventDefault();
+  let apiKey = "156d56571be94f1383a53abf8e9ae72f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
+
+  axios.get(`${apiUrl}&units=metric`).then(temperature);
+}
+let currentLocationButton = document.querySelector("#location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
