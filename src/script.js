71a30b0bb4d34d22be82698462faae997e9e5dea
Faay97 <101605402+Faay97@users.noreply.github.com>
2022-04-08 03:25:08 -0400

    added appropriate icon
    icon's now match the city temperature

--- src/script.js
+++ src/script.js
@@ -52,13 +52,19 @@ function temperature(response) {
   document.querySelector("#humidity").innerHTML = Math.round(
     response.data.main.humidity
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(retrieveLocation);
 }
 function retrieveLocation(position) {
-  //posit ion.preventDefault();
+  //position.preventDefault();
   let apiKey = "156d56571be94f1383a53abf8e9ae72f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
 
